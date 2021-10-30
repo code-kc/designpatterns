aaa5a9498d5106cc52a6c1722b87e4517b4720b0
codeavinash <avi.ec24@gmail.com>
2021-10-30 16:39:44 +0530

    Add project files.

---
+++ src/DesignPatterns/StrategyPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using StrategyPattern.Business;
+
+var order = new Order
+{
+    ShippingDetails = new ShippingDetails
+    {
+        OriginCountry = "Sweden",
+        DestinationCountry = "Sweden"
+    }
+};
+
+order.LineItems.Add(new Item("CSHARP_SMORGASBORD", "C# Smorgasbord", 100m, ItemType.Literature), 1);
+order.LineItems.Add(new Item("CONSULTING", "Building a website", 100m, ItemType.Service), 1);
+
+Console.WriteLine(order.GetTax());
